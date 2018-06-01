8e3fbfccec654369ca303f05382f85a9dda0ceca
user <user@Yujin>
2018-06-01 18:52:16 +0900

    mission

---
+++ src/Gugudan_homework2.java
@@ -0,0 +1,14 @@
+//사용자가 입력한 값에 따라 크기가 다른 구구단을 계산해 출력한다.
+// 1. 사용자가 8을 입력하면 8*8단, 19를 입력하면 19*19(2*1 ~ 19*19)를 계산해 출력한다.
+// 2. 사용자가 "8,7"과 같은 문자열을 입력하면 팔칠단을 구현한다.(2*1 ~ 8*7) 
+
+import java.util.Scanner;
+
+public class Gugudan_homework2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
