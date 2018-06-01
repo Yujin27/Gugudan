df238645543ed5c89b09a3041cacb204bbf1ece2
user <user@Yujin>
2018-06-01 18:24:23 +0900

    create class

--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -3,7 +3,6 @@ public class GugudanMain {
 	public static void main(String[] args) {
 		
 		for(int j =2; j <10; j++) {
-			//calculate와 print 메소드 활용해 구구단 구현하기	
 			int [] result = Gugudan.calculate(j);
 			Gugudan.print(result);
 		}		
